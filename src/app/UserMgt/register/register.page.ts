b09952f791cc859363dd1fb0381622f82676d501
Steve Njoroge <njorogesteve31@gmail.com>
2020-10-13 19:28:14 +0300

    First Design Done'

---
+++ src/app/UserMgt/register/register.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  constructor(
+    public loadingController: LoadingController, 
+    public router: Router
+  ) { }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Please wait...',
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed!');
+    await this.router.navigate(["/login"]);
+  }
+
+  ngOnInit() {
+  }
+
+}
